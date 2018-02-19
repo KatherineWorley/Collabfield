d5fb571d1c81e42bc01ae3c48b216e04f9b8ea38
Katherine Worley <kdesign216@yahoo.com>
2018-02-19 09:27:01 -0500

    Added column to users table and updated name attributes inside reg controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
   root to: 'pages#index'
 
-    devise_scope :user do
-	  get 'login', to: 'devise/sessions#new'
-	end
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  
 end
