686598bdb939fbcf311e8fce277705db767167c6
Katherine Worley <kdesign216@yahoo.com>
2018-02-19 09:37:51 -0500

    changed signup route page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
-  
+
+  devise_scope :user do
+  	get 'signup', to: 'devise/registrations#new'
+  end
+
 end
