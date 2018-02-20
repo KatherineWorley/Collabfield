00ffaf8da07550a16843b3f3c7c723e98c7786dd
Katherine Worley <kdesign216@yahoo.com>
2018-02-20 10:25:59 -0500

    updated order conversations

--- app/controllers/order_conversations_service_controller.rb
+++ app/controllers/order_conversations_service_controller.rb
@@ -5,8 +5,7 @@ class OrderConversationsService
 
   # get and order conversations by last messages' dates in descending order
   def call
-    all_private_conversations = Private::Conversation.all_by_user(@user.id)
-                                                     .includes(:messages)
+    all_private_conversations = Private::Conversation.all_by_user(@user.id).includes(:messages)
     return all_conversations = all_private_conversations.sort{ |a, b| 
       b.messages.last.created_at <=> a.messages.last.created_at
     }
