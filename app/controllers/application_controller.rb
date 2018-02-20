8683b61d8bd7ab3af5b6d94d1a5868360096ba2f
Katherine Worley <kdesign216@yahoo.com>
2018-02-20 08:42:18 -0500

    added OrderConversationsService

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :all_ordered_conversations
+
+  def all_ordered_conversations 
+	  if user_signed_in?
+	    @all_conversations = OrderConversationsService.new({user: current_user}).call
+	  end
+  end
 end
