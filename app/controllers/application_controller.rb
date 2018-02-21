ef48ce5fdcd40377d46d0513157cb0d15d41ef81
Katherine Worley <kdesign216@yahoo.com>
2018-02-21 08:25:27 -0500

    working on devise bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :all_ordered_conversations
-  before_action :is_signed_in?
+  before_action :user_signed_in?
 
   def all_ordered_conversations 
 	  if user_signed_in?
