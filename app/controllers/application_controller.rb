c1bf6c231a470859872d1bb6d3c4415bd96d8620
Katherine Worley <kdesign216@yahoo.com>
2018-02-22 07:42:25 -0500

    working with devise method issue. Learned Devise.mappings.keys, cool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :all_ordered_conversations
   before_action :user_signed_in?
+  before_action :all_ordered_conversations
 
   def all_ordered_conversations 
 	  if user_signed_in?
