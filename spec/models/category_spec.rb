9baf4e69f7488429f7169475d96e1a7f45566403
Katherine Worley <kdesign216@yahoo.com>
2018-02-28 10:11:28 -0500

    added bootstrap layout to home page, added css and responsive design changes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it 'has_many posts' do
+      association = described_class.reflect_on_association(:posts)
+      expect(association.macro).to eq :has_many
+    end
+  end
 end
