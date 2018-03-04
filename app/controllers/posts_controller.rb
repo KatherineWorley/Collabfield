ba93db82166ab5748eaabe369b87c66116f0fad8
Katherine Worley <kdesign216@yahoo.com>
2018-03-04 10:02:59 -0500

    added more in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
-	def show
-    	@post = Post.find(params[:id])
-  	end
+  def show
+   	@post = Post.find(params[:id])
+  end
+
+  def hobby
+   	posts_for_branch(params[:action])
+  end
+
+  def study
+    posts_for_branch(params[:action])
+  end
+
+  def team
+    posts_for_branch(params[:action])
+  end
 end
