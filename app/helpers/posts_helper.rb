1f99d1ceb5ae2478d2be5036c21be9a1374cae09
Katherine Worley <kdesign216@yahoo.com>
2018-03-06 09:58:24 -0500

    Add posts from all branches in the home page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -33,10 +33,14 @@ module PostsHelper
   end
   
   def update_pagination_partial_path
-  if @posts.next_page
-    'posts/posts_pagination_page/update_pagination'
-  else
-    'posts/posts_pagination_page/remove_pagination'
+    if @posts.next_page
+      'posts/posts_pagination_page/update_pagination'
+    else
+      'posts/posts_pagination_page/remove_pagination'
+    end
   end
-end
+
+  def no_posts_partial_path(posts)
+    posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
+  end
 end
