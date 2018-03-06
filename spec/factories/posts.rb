a5d7e0f5f15a2702c3e4cecdf0ba2b1267e907b5
Katherine Worley <kdesign216@yahoo.com>
2018-03-06 09:29:40 -0500

    Make search bar and category filters in branch pages functional

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -5,4 +5,16 @@ FactoryBot.define do
     user
     category
   end
+
+  scope :by_category, -> (branch, category_name) do 
+    joins(:category).where(categories: {name: category_name, branch: branch}) 
+  end
+
+  scope :by_branch, -> (branch) do
+    joins(:category).where(categories: {branch: branch}) 
+  end
+
+  scope :search, -> (search) do
+    where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
+  end
 end
