c395bda4ebd3ca5e5488f4663fc3837f386d3383
Katherine Worley <kdesign216@yahoo.com>
2018-03-06 10:08:08 -0500

    updated posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+  
   def show
    	@post = Post.find(params[:id])
   end
