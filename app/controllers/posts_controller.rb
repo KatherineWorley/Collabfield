65f1727942fb8dbe3e38dcfa64c511e7c770e44f
Katherine Worley <kdesign216@yahoo.com>
2018-03-07 16:37:02 -0500

    updated posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,10 @@ class PostsController < ApplicationController
   
   def show
    	@post = Post.find(params[:id])
+    
+    if user_signed_in?
+      @message_has_been_sent = conversation_exist?
+    end
   end
 
   def hobby
