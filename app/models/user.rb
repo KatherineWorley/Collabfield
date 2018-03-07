a9e020404bb990a13c52036a6bc0bce73e2d26b2
Katherine Worley <kdesign216@yahoo.com>
2018-03-07 15:59:48 -0500

    Create Private::Conversation and Private::Message models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
+
+  has_many :private_messages, class_name: 'Private::Message'
+  has_many  :private_conversations, 
+          foreign_key: :sender_id, 
+          class_name: 'Private::Conversation'
 end
