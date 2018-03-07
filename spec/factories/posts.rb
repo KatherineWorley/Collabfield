a3926f6b95612a85ecccb780dc24ce9b7a8a1310
Katherine Worley <kdesign216@yahoo.com>
2018-03-07 15:26:01 -0500

    Add validations to the Post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do 
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :category_id, presence: true
+
   factory :post do
     title 'a' * 20
     content 'a' * 20
